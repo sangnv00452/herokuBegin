62d90cbbb57a0a29a81026aebbf3f06ccf4014f3
sangnv00452 <sangnv00452@gmail.com>
2014-03-14 15:29:31 +0700

    update

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php echo "Hello" ?>
+
+<!DOCTYPE html>
+<html xmlns:fb="http://ogp.me/ns/fb#" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
+    <script type="text/javascript" src="/javascript/jquery-1.7.1.min.js"></script>
+
+  </head>
+  <body>
+    <h1>Hello</h1>
+
+
+  </body>
+</html>
